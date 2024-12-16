8d86587814c3da4f80c8eacf2c9eda6236c7381d
Bilal <153359274+Babaevskiyy@users.noreply.github.com>
2024-12-16 16:53:41 +0300

    Update wait-for-it.sh

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -1,182 +1,88 @@
-#!/usr/bin/env bash
-# Use this script to test if a given TCP host/port are available
+package services
 
-WAITFORIT_cmdname=${0##*/}
+import (
+	"fmt"
+	"strconv"
 
-echoerr() { if [[ $WAITFORIT_QUIET -ne 1 ]]; then echo "$@" 1>&2; fi }
+	"github.com/xuri/excelize/v2"
+)
 
-usage()
-{
-    cat << USAGE >&2
-Usage:
-    $WAITFORIT_cmdname host:port [-s] [-t timeout] [-- command args]
-    -h HOST | --host=HOST       Host or IP under test
-    -p PORT | --port=PORT       TCP port under test
-                                Alternatively, you specify the host and port as host:port
-    -s | --strict               Only execute subcommand if the test succeeds
-    -q | --quiet                Don't output any status messages
-    -t TIMEOUT | --timeout=TIMEOUT
-                                Timeout in seconds, zero for no timeout
-    -- COMMAND ARGS             Execute command with args after the test finishes
-USAGE
-    exit 1
+type ExportItem struct {
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	ClearedName string `json:"cleared_name"`
+
+	Rule1             bool   `json:"rule_1"`
+	Rule2Description  string `json:"rule_2_description"`
+	Rule3Description  string `json:"rule_3_description"`
+	Rule4Description  string `json:"rule_4_description"`
+	Rule5Description  string `json:"rule_5_description"`
+	Rule6Description  string `json:"rule_6_description"`
+	Rule7Description  string `json:"rule_7_description"`
+	Rule8Description  string `json:"rule_8_description"`
+	Rule9Description  string `json:"rule_9_description"`
+	Rule10Description string `json:"rule_10_description"`
+	Rule17Description string `json:"rule_17_description"`
 }
 
-wait_for()
-{
-    if [[ $WAITFORIT_TIMEOUT -gt 0 ]]; then
-        echoerr "$WAITFORIT_cmdname: waiting $WAITFORIT_TIMEOUT seconds for $WAITFORIT_HOST:$WAITFORIT_PORT"
-    else
-        echoerr "$WAITFORIT_cmdname: waiting for $WAITFORIT_HOST:$WAITFORIT_PORT without a timeout"
-    fi
-    WAITFORIT_start_ts=$(date +%s)
-    while :
-    do
-        if [[ $WAITFORIT_ISBUSY -eq 1 ]]; then
-            nc -z $WAITFORIT_HOST $WAITFORIT_PORT
-            WAITFORIT_result=$?
-        else
-            (echo -n > /dev/tcp/$WAITFORIT_HOST/$WAITFORIT_PORT) >/dev/null 2>&1
-            WAITFORIT_result=$?
-        fi
-        if [[ $WAITFORIT_result -eq 0 ]]; then
-            WAITFORIT_end_ts=$(date +%s)
-            echoerr "$WAITFORIT_cmdname: $WAITFORIT_HOST:$WAITFORIT_PORT is available after $((WAITFORIT_end_ts - WAITFORIT_start_ts)) seconds"
-            break
-        fi
-        sleep 1
-    done
-    return $WAITFORIT_result
+func ExportItemsToXlsx(filename string, items []ExportItem) {
+	f := excelize.NewFile()
+	defer func() {
+		if err := f.Close(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	index, err := f.NewSheet("Sheet1")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// TODO: Реализовать автоматическую подстановку нужной буквы
+	f.SetCellValue("Sheet1", "A1", "Код")
+	f.SetCellValue("Sheet1", "B1", "Наименование")
+	f.SetCellValue("Sheet1", "C1", "Нормализованное наименование")
+	f.SetCellValue("Sheet1", "D1", "Правило 1")
+	f.SetCellValue("Sheet1", "E1", "Правило 2")
+	f.SetCellValue("Sheet1", "F1", "Правило 3")
+	f.SetCellValue("Sheet1", "G1", "Правило 4")
+	f.SetCellValue("Sheet1", "H1", "Правило 5")
+	f.SetCellValue("Sheet1", "I1", "Правило 6")
+	f.SetCellValue("Sheet1", "J1", "Правило 7")
+	f.SetCellValue("Sheet1", "K1", "Правило 8")
+	f.SetCellValue("Sheet1", "L1", "Правило 9")
+	f.SetCellValue("Sheet1", "M1", "Правило 10")
+	f.SetCellValue("Sheet1", "N1", "Правило 17")
+	currItem := 1
+	for _, item := range items {
+		currItem++
+		currentItemStr := strconv.Itoa(currItem)
+		f.SetCellValue("Sheet1", "A"+currentItemStr, item.Id)
+		f.SetCellValue("Sheet1", "B"+currentItemStr, item.Name)
+		f.SetCellValue("Sheet1", "C"+currentItemStr, item.ClearedName)
+		strRule1 := "Запись нормальная"
+		if !item.Rule1 {
+			strRule1 = "Запись содержит ошибку"
+		}
+		f.SetCellValue("Sheet1", "D"+currentItemStr, strRule1)
+		f.SetCellValue("Sheet1", "E"+currentItemStr, item.Rule2Description)
+		f.SetCellValue("Sheet1", "F"+currentItemStr, item.Rule3Description)
+		f.SetCellValue("Sheet1", "G"+currentItemStr, item.Rule4Description)
+		f.SetCellValue("Sheet1", "H"+currentItemStr, item.Rule5Description)
+		f.SetCellValue("Sheet1", "I"+currentItemStr, item.Rule6Description)
+		f.SetCellValue("Sheet1", "J"+currentItemStr, item.Rule7Description)
+		f.SetCellValue("Sheet1", "K"+currentItemStr, item.Rule8Description)
+		f.SetCellValue("Sheet1", "L"+currentItemStr, item.Rule9Description)
+		f.SetCellValue("Sheet1", "M"+currentItemStr, item.Rule10Description)
+		f.SetCellValue("Sheet1", "N"+currentItemStr, item.Rule17Description)
+	}
+
+	f.SetColWidth("Sheet1", "A", "M", 20)
+
+	f.SetActiveSheet(index)
+
+	if err := f.SaveAs(filename); err != nil {
+		fmt.Println(err)
+	}
 }
-
-wait_for_wrapper()
-{
-    # In order to support SIGINT during timeout: http://unix.stackexchange.com/a/57692
-    if [[ $WAITFORIT_QUIET -eq 1 ]]; then
-        timeout $WAITFORIT_BUSYTIMEFLAG $WAITFORIT_TIMEOUT $0 --quiet --child --host=$WAITFORIT_HOST --port=$WAITFORIT_PORT --timeout=$WAITFORIT_TIMEOUT &
-    else
-        timeout $WAITFORIT_BUSYTIMEFLAG $WAITFORIT_TIMEOUT $0 --child --host=$WAITFORIT_HOST --port=$WAITFORIT_PORT --timeout=$WAITFORIT_TIMEOUT &
-    fi
-    WAITFORIT_PID=$!
-    trap "kill -INT -$WAITFORIT_PID" INT
-    wait $WAITFORIT_PID
-    WAITFORIT_RESULT=$?
-    if [[ $WAITFORIT_RESULT -ne 0 ]]; then
-        echoerr "$WAITFORIT_cmdname: timeout occurred after waiting $WAITFORIT_TIMEOUT seconds for $WAITFORIT_HOST:$WAITFORIT_PORT"
-    fi
-    return $WAITFORIT_RESULT
-}
-
-# process arguments
-while [[ $# -gt 0 ]]
-do
-    case "$1" in
-        *:* )
-        WAITFORIT_hostport=(${1//:/ })
-        WAITFORIT_HOST=${WAITFORIT_hostport[0]}
-        WAITFORIT_PORT=${WAITFORIT_hostport[1]}
-        shift 1
-        ;;
-        --child)
-        WAITFORIT_CHILD=1
-        shift 1
-        ;;
-        -q | --quiet)
-        WAITFORIT_QUIET=1
-        shift 1
-        ;;
-        -s | --strict)
-        WAITFORIT_STRICT=1
-        shift 1
-        ;;
-        -h)
-        WAITFORIT_HOST="$2"
-        if [[ $WAITFORIT_HOST == "" ]]; then break; fi
-        shift 2
-        ;;
-        --host=*)
-        WAITFORIT_HOST="${1#*=}"
-        shift 1
-        ;;
-        -p)
-        WAITFORIT_PORT="$2"
-        if [[ $WAITFORIT_PORT == "" ]]; then break; fi
-        shift 2
-        ;;
-        --port=*)
-        WAITFORIT_PORT="${1#*=}"
-        shift 1
-        ;;
-        -t)
-        WAITFORIT_TIMEOUT="$2"
-        if [[ $WAITFORIT_TIMEOUT == "" ]]; then break; fi
-        shift 2
-        ;;
-        --timeout=*)
-        WAITFORIT_TIMEOUT="${1#*=}"
-        shift 1
-        ;;
-        --)
-        shift
-        WAITFORIT_CLI=("$@")
-        break
-        ;;
-        --help)
-        usage
-        ;;
-        *)
-        echoerr "Unknown argument: $1"
-        usage
-        ;;
-    esac
-done
-
-if [[ "$WAITFORIT_HOST" == "" || "$WAITFORIT_PORT" == "" ]]; then
-    echoerr "Error: you need to provide a host and port to test."
-    usage
-fi
-
-WAITFORIT_TIMEOUT=${WAITFORIT_TIMEOUT:-15}
-WAITFORIT_STRICT=${WAITFORIT_STRICT:-0}
-WAITFORIT_CHILD=${WAITFORIT_CHILD:-0}
-WAITFORIT_QUIET=${WAITFORIT_QUIET:-0}
-
-# Check to see if timeout is from busybox?
-WAITFORIT_TIMEOUT_PATH=$(type -p timeout)
-WAITFORIT_TIMEOUT_PATH=$(realpath $WAITFORIT_TIMEOUT_PATH 2>/dev/null || readlink -f $WAITFORIT_TIMEOUT_PATH)
-
-WAITFORIT_BUSYTIMEFLAG=""
-if [[ $WAITFORIT_TIMEOUT_PATH =~ "busybox" ]]; then
-    WAITFORIT_ISBUSY=1
-    # Check if busybox timeout uses -t flag
-    # (recent Alpine versions don't support -t anymore)
-    if timeout &>/dev/stdout | grep -q -e '-t '; then
-        WAITFORIT_BUSYTIMEFLAG="-t"
-    fi
-else
-    WAITFORIT_ISBUSY=0
-fi
-
-if [[ $WAITFORIT_CHILD -gt 0 ]]; then
-    wait_for
-    WAITFORIT_RESULT=$?
-    exit $WAITFORIT_RESULT
-else
-    if [[ $WAITFORIT_TIMEOUT -gt 0 ]]; then
-        wait_for_wrapper
-        WAITFORIT_RESULT=$?
-    else
-        wait_for
-        WAITFORIT_RESULT=$?
-    fi
-fi
-
-if [[ $WAITFORIT_CLI != "" ]]; then
-    if [[ $WAITFORIT_RESULT -ne 0 && $WAITFORIT_STRICT -eq 1 ]]; then
-        echoerr "$WAITFORIT_cmdname: strict mode, refusing to execute subprocess"
-        exit $WAITFORIT_RESULT
-    fi
-    exec "${WAITFORIT_CLI[@]}"
-else
-    exit $WAITFORIT_RESULT
-fi
